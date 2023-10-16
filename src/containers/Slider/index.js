847c6cdd3e77178bd4440dbc5ae135f6027a7312
Marie57412 <mariebraun1@outlook.fr>
2023-10-16 10:34:25 +0200

    finalisation

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,24 +5,32 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
+  // Récupération des données à partir du contexte Data.
   const { data } = useData();
+  // Initialisation de l'index de la carte actuellement affichée.
   const [index, setIndex] = useState(0);
+
+  // Tri des événements par date décroissante.
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
- 
+  // Fonction pour passer à la carte suivante.
   const nextCard = () => {
-    if(byDateDesc){
-     
-    setTimeout(
-      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
-      5000
-    );}
+    if (byDateDesc) {
+      // Utilisation d'un délai de 5000 ms (5 secondes) pour passer à la carte suivante. et ajout de -1 car il n'y a que 3 images
+      setTimeout(
+        () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+        5000
+      );
+    }
   };
+
+  // Utilisation de useEffect pour déclencher automatiquement nextCard au chargement de la composante.
   useEffect(() => {
     nextCard();
   });
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
