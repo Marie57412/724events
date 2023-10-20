f106914d2389a824fccaadd974e0022e499da173
Marie57412 <mariebraun1@outlook.fr>
2023-10-20 13:46:51 +0200

    test additionnel

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -2,14 +2,19 @@
  * 
  */
 
-describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
-})
+import { getMonth } from ".";
 
+describe("Date helper", () => {
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      const result = getMonth(new Date("2022-01-01"));
+      expect(result).toBe("janvier");
+      // to implement
+    });
+    it("the function return juillet for 2022-07-08 as date", () => {
+      const result = getMonth(new Date("2022-07-08"));
+      expect(result).toBe("juillet");
+      // to implement
+    });
+  });
+});
